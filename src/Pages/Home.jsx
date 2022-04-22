9df7a25a8d246055b35154e1a74776e86b76f6a0
Mariana Lourenço <marianasdlourenco@gmail.com>
2022-04-22 18:50:25 -0300

    feat : adding mdb react ui

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
-const Home = () => {
-  return <div>Página inicial</div>;
-};
+import React from 'react';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+
+function Home() {
+  return (
+    <div>Página inicial</div>
+  );
+}
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import AppRouter from './routes';
 
 import reportWebVitals from './reportWebVitals';
