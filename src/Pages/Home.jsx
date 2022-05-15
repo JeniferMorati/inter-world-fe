3fdb6b321b77b3e7d85fe20a3fab7663145bd88f
Mariana Lourenço <marianasdlourenco@gmail.com>
2022-05-15 18:30:56 -0300

    Faq and Gallery

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,218 @@
 import Footer from "../Footer/Footer";
-
 function Home() {
   return (
+  <>
+  
+<section>
+  <h3 class="text-center mb-4 pb-2 text-secondary fw-bold">Conheça os principais destinos!</h3>
+    <div class="row">
+      <div class="col-lg-4 col-md-12 mb-4">
+      <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://mdbcdn.b-cdn.net/img/new/fluid/city/055.webp"
+            class="w-100" height="250" />
+            <div class="mask">
+              <div class="d-flex justify-content-start align-items-start h-100">
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask"></div>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp"
+            class="w-100" height="250"/>
+            <div class="mask">
+              <div class="d-flex justify-content-start align-items-start h-100">
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask"></div>
+            </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+            class="w-100" height="250" />
+            <div class="mask">
+              <div class="d-flex justify-content-start align-items-start h-100">
+              </div>
+            </div>
+            <div class="hover-overlay">
+              <div class="mask"></div>
+            </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section>
+  <h3 class="text-center mb-4 pb-2 text-secondary fw-bold">FAQ</h3>
+  <p class="text-center mb-5">
+  Encontre abaixo as respostas para as perguntas mais frequentes
+  </p>
+
+  <div class="row">
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-secondary"><i class="far fa-paper-plane text-secondary pe-2"></i>
+       Que documentos pessoais necessito para viajar?
+</h6>
+      <p>
+      Voucher de Reserva, Documento original com foto (RG, CNH)
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-secondary"><i class="fas fa-pen-alt text-secondary pe-2"></i> 
+      Preciso de visto?</h6>
+      <p>
+        Depende do destino da sua viagem!
+         Dentro da União Europeia e na America é necessario cartão de cidadão válido e passaporte valido.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-secondary"><i class="fas fa-user text-secondary pe-2"></i> 
+      Vou viajar com um menor. Há alguma especificidade que deva considerar?
+      </h6>
+      <p>
+      Sim, deve conter uma autorização de saída do país sempre que viajarem.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-secondary"><i class="fas fa-rocket text-secondary pe-2"></i> 
+      É preciso ir até agência para comprar alguma viagem?
+      </h6>
+      <p>
+      Não, Somos uma plataforma 100% digital.
+</p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-secondary"><i class="fas fa-home text-secondary pe-2"></i> Como faço meu Orçamento?
+      </h6>
+      <p> Clique na pagina Orçamento e preencha os campos com seus dados.</p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-secondary"><i class="fas fa-book-open text-secondary pe-2"></i>
+      Onde posso encontrar todos os Destinos?</h6>
+      <p> Clique na Pagina Destinos para conhecer todos os Paises!</p>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div class="row d-flex justify-content-center">
+    <div class="col-md-10 col-xl-8 text-center">
+      <h3 class="mb-4">Testemunhos</h3>
+      <p class="mb-4 pb-2 mb-md-5 pb-md-0">
+        Conheça alguns depoimentos de pessoas que 
+        realizaram seu sonho de viagem pelo mundo pela nossa agência.
+      </p>
+    </div>
+  </div>
+
+  <div class="row text-center">
+    <div class="col-md-4 mb-5 mb-md-0">
+      <div class="d-flex justify-content-center mb-4">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+          class="rounded-circle shadow-1-strong" width="150" height="150" />
+      </div>
+      <h5 class="mb-3">Maria Smantha</h5>
+      <h6 class="text-secondary mb-3">Desenvolvedor Web</h6>
+      <p class="px-xl-3">
+        <i class="fas fa-quote-left pe-2"></i> Muito bom, me ajudou há realizar meu sonho de viajar
+        para o Canada, 5 estrelas!
+      </p>
+      <ul class="list-unstyled d-flex justify-content-center mb-0">
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star-half-alt fa-sm text-warning"></i>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-4 mb-5 mb-md-0">
+      <div class="d-flex justify-content-center mb-4">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+          class="rounded-circle shadow-1-strong" width="150" height="150" />
+      </div>
+      <h5 class="mb-3">Lisa Cudrow</h5>
+      <h6 class="text-secondary mb-3">Designer gráfico</h6>
+      <p class="px-xl-3">
+        <i class="fas fa-quote-left pe-2"></i> Otimo site para realizar sua viagem para 
+        o exterior, atendimento rapido e orçamento bem planejado.
+      </p>
+      <ul class="list-unstyled d-flex justify-content-center mb-0">
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-4 mb-0">
+      <div class="d-flex justify-content-center mb-4">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+          class="rounded-circle shadow-1-strong" width="150" height="150" />
+      </div>
+      <h5 class="mb-3">John Smith</h5>
+      <h6 class="text-secondary mb-3">Especialista em Marketing</h6>
+      <p class="px-xl-3">
+        <i class="fas fa-quote-left pe-2"></i> fizemos uma otima viagem para o Estados Unidos
+        graças ao site, uma otima oporturnidade para realizar seu sonho de viagem pelo mundo, 5 estrelas,
+        obrigado!
+      </p>
+      <ul class="list-unstyled d-flex justify-content-center mb-0">
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="fas fa-star fa-sm text-warning"></i>
+        </li>
+        <li>
+          <i class="far fa-star fa-sm text-warning"></i>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+
+
 <Footer/>
+</>
   );
 }
 export default Home;
